215c9280c02547c1978fbeb21a0ae959d9918994
jeffry21 <jeffryfikom19@gmail.com>
2025-05-27 00:20:21 +0800

    env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
+  dialectModule: mysql2,
   // dialectOptions: {
   //   ssl: {
   //     require: true,
