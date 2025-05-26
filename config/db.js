aab0d7353512d12f7214bcb931fb46916f004a11
jeffry21 <jeffryfikom19@gmail.com>
2025-05-26 23:35:27 +0800

    Deskripsikan perubahan di sini

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('db_sekolah', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+// const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize('db_sekolah', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
+// module.exports = sequelize;
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
 });
-module.exports = sequelize;
